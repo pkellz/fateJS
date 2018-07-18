3c33de0f624bedd79ee5f86b4824e7ca4ed3798a
pkellz <pkellz@github.com>
2018-07-18 16:04:23 -0400

    Finishing up .bool

--- fate.js
+++ fate.js
@@ -1,7 +1,6 @@
-function fate(options)
+function fate()
 {
   const self = {}
-  if(options) self.options = options
   self.print = function()
   {
     console.log(self);
@@ -15,7 +14,13 @@ function fate(options)
   {
       return min == null || max == null ? Math.random() :
             Math.random() * max + min
-
+  }
+  //True 50% of the time unless a chance is passed in
+  self.bool = function(options)
+  {
+    options ? chance = options.chance : 1
+    let c = (chance != null) ? 1 - (chance / 100) : 0.5
+    return Math.random() > c
   }
   return self
 }
