7a89b1585aef5bba07190d4ef87c9619ea60d202
pkellz <pkellz@github.com>
2018-07-18 16:17:35 -0400

    Completing .letter()

--- fate.js
+++ fate.js
@@ -18,9 +18,18 @@ function fate()
   //True 50% of the time unless a chance is passed in
   self.bool = function(options)
   {
-    options ? chance = options.chance : 1
+    options ? chance = options.chance : 0
     let c = (chance != null) ? 1 - (chance / 100) : 0.5
     return Math.random() > c
+  }
+  self.letter = function(options)
+  {
+    options ? casing = options.casing : 0
+    console.log(casing);
+    const alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
+                  'n','o','p','q','r','s','t','u','v','w','x','y','z']
+    const char = alpha[Math.floor(Math.random()*alpha.length+1)]
+    return Math.random() > 0.5 ? char.toLowerCase() : char.toUpperCase()
   }
   return self
 }
